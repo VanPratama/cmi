9d5f6fd2f33f384a79098d045cae7fe09299d45c
VanP <p.ivan000.ip@gmail.com>
2019-04-12 14:52:19 +0700

    -new

---
+++ formpenjualan.php
@@ -0,0 +1,163 @@
+<div class="row pt-4 mx-0 mb-1">
+	<div class="col mr-1 py-5 px-0 bg-light" style="border-radius: 5px;">
+		<ul>
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">COA:</span>
+			  </div>
+			  <input type="text" class="form-control" name="coa" placeholder="Masukkan COA">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Customer:</span>
+			  </div>
+			  <input type="text" class="form-control" name="customer" placeholder="Masukkan nama Customer">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Nama Barang:</span>
+			  </div>
+			  <input type="text" class="form-control" name="barang" placeholder="Masukkan Nama Barang">
+			 </div>
+			 </div>
+			</div>
+
+		</ul>
+	</div>
+
+	<div class="col ml-1 py-5 px-0 bg-light" style="border-radius: 5px;">
+		<ul>
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">SPK Date:</span>
+			  </div>
+			  <input type="date" class="form-control" name="spkdate">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">No. SPK:</span>
+			  </div>
+			  <input type="text" class="form-control" name="nospk" placeholder="Masukkan Nomor SPK">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">[SPK] Quantity:</span>
+			  </div>
+			  <input type="text" class="form-control" name="qty" placeholder="Masukkan jumlah barang">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Hasil:</span>
+			  </div>
+			  <input type="text" class="form-control" name="hasil" placeholder="Masukkan Hasil">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Selisih:</span>
+			  </div>
+			  <input type="text" class="form-control" name="selisih" placeholder="Masukkan Jumlah Selisih">
+			 </div>
+			 </div>
+			</div>								
+		</ul>
+	</div>
+</div>
+
+<div class="row pt-2 mx-0 mb-1">
+	<div class="col mr-1 py-5 px-0 bg-light" style="border-radius: 5px;">
+		<ul>
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Invoice Date:</span>
+			  </div>
+			  <input type="date" class="form-control" name="invoicedate">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">No.Invoice:</span>
+			  </div>
+			  <input type="text" class="form-control" name="nomorinvoice" placeholder="Masukkan Nomor Invoice">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">[Invoice] Quantity:</span>
+			  </div>
+			  <input type="text" class="form-control" name="nomorinvoice" placeholder="Masukkan Jumlah Barang">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">P/U:</span>
+			  </div>
+			  <input type="text" class="form-control" name="pu" placeholder="Masukkan P/U">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Nominal:</span>
+			  </div>
+			  <input type="text" class="form-control" name="nominal" placeholder="Masukkan Nominal">
+			 </div>
+			 </div>
+			</div>
+		</ul>
+	</div>
+
+	<div class="col ml-1 py-5 px-0 bg-light" style="border-radius: 5px;">
+
+	</div>
+</div>
