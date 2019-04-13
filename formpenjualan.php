bc9cf8036d5ec497ad7a8dc68f1f278cd3a5dc0e
VanP <p.ivan000.ip@gmail.com>
2019-04-13 10:59:53 +0700

    -update blueprint

--- formpenjualan.php
+++ formpenjualan.php
@@ -1,5 +1,5 @@
-<div class="row pt-4 mx-0 mb-1">
-	<div class="col mr-1 py-5 px-0 bg-light" style="border-radius: 5px;">
+<div class="row pt-4 mx-0">
+	<div class="col mr-1 mb-1 py-5 px-0 bg-light" style="border-radius: 5px;">
 		<ul>
 			<div class="form-group">
 			 <div class="col-sm-10">
@@ -7,7 +7,7 @@
 			  <div class="input-group-prepend">
 			    <span class="input-group-text">COA:</span>
 			  </div>
-			  <input type="text" class="form-control" name="coa" placeholder="Masukkan COA">
+			  <input type="text" class="form-control" name="coa" placeholder="Masukkan Nomor COA">
 			 </div>
 			 </div>
 			</div>
@@ -18,7 +18,7 @@
 			  <div class="input-group-prepend">
 			    <span class="input-group-text">Customer:</span>
 			  </div>
-			  <input type="text" class="form-control" name="customer" placeholder="Masukkan nama Customer">
+			  <input type="text" class="form-control" name="customer" placeholder="Masukkan Nama Customer">
 			 </div>
 			 </div>
 			</div>
@@ -37,7 +37,7 @@
 		</ul>
 	</div>
 
-	<div class="col ml-1 py-5 px-0 bg-light" style="border-radius: 5px;">
+	<div class="col mb-1 py-5 px-0 bg-light" style="border-radius: 5px;">
 		<ul>
 			<div class="form-group">
 			 <div class="col-sm-10">
@@ -67,7 +67,7 @@
 			  <div class="input-group-prepend">
 			    <span class="input-group-text">[SPK] Quantity:</span>
 			  </div>
-			  <input type="text" class="form-control" name="qty" placeholder="Masukkan jumlah barang">
+			  <input type="text" class="form-control" name="qty" placeholder="Masukkan Jumlah barang">
 			 </div>
 			 </div>
 			</div>
@@ -97,8 +97,8 @@
 	</div>
 </div>
 
-<div class="row pt-2 mx-0 mb-1">
-	<div class="col mr-1 py-5 px-0 bg-light" style="border-radius: 5px;">
+<div class="row mx-0">
+	<div class="col mr-1 mb-1 py-5 px-0 bg-light" style="border-radius: 5px;">
 		<ul>
 			<div class="form-group">
 			 <div class="col-sm-10">
@@ -157,7 +157,158 @@
 		</ul>
 	</div>
 
-	<div class="col ml-1 py-5 px-0 bg-light" style="border-radius: 5px;">
+	<div class="col mb-1 py-5 px-0 bg-light" style="border-radius: 5px;">
+		<ul>
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Tanggal Pembayaran:</span>
+			  </div>
+			  <input type="date" class="form-control" name="tglbayar">
+			 </div>
+			 </div>
+			</div>
 
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Metode Pembayaran:</span>
+			  </div>
+			  <input type="text" class="form-control" name="pembayaran" placeholder="Masukkan Metode Pembayaran [Cash,BCA,BRI,etc]">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Total Pembayaran:</span>
+			  </div>
+			  <input type="text" class="form-control" name="totalbayar" placeholder="Masukkan Total Pembayaran">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Saldo Pembayaran:</span>
+			  </div>
+			  <input type="text" class="form-control" name="saldobayar" placeholder="Masukkan Saldo">
+			 </div>
+			 </div>
+			</div>		
+		</ul>
 	</div>
+</div>
+
+<div class="row mx-0">
+	<div class="col mr-1 mb-1 py-5 px-0 bg-light" style="border-radius: 5px;">
+		<ul>
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Mesin Besar:</span>
+			  </div>
+			  <input type="text" class="form-control" name="mesinbesar" placeholder="Hasil Mesin Besar">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Mesin Kecil:</span>
+			  </div>
+			  <input type="text" class="form-control" name="mesinkecil" placeholder="Hasil Mesin Kecil">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">HPP:</span>
+			  </div>
+			  <input type="text" class="form-control" name="hpp" placeholder="Masukkan HPP">
+			 </div>
+			 </div>
+			</div>			
+		</ul>
+	</div>
+
+	<div class="col mb-1 py-5 px-0 bg-light" style="border-radius: 5px;">
+		<ul>
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Shift Pagi:</span>
+			  </div>
+			  <input type="text" class="form-control" name="spagi" placeholder="Masukkan Nama Orang di Shift Pagi">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Shift Malam:</span>
+			  </div>
+			  <input type="text" class="form-control" name="smalam" placeholder="Masukkan Nama Orang di Shift Malam">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Shift Sabtu Pagi:</span>
+			  </div>
+			  <input type="text" class="form-control" name="sspagi" placeholder="Nama Orang di Shift Sabtu Pagi">
+			 </div>
+			 </div>
+			</div>
+
+			<div class="form-group">
+			 <div class="col-sm-10">
+			 <div class="input-group mb-3 input-group-sm">
+			  <div class="input-group-prepend">
+			    <span class="input-group-text">Shift Sabtu Malam:</span>
+			  </div>
+			  <input type="text" class="form-control" name="ssmalam" placeholder="Nama Orang di Shift Sabtu Malam">
+			 </div>
+			 </div>
+			</div>
+		</ul>
+	</div>
+
+</div>
+
+<div class="row pt-4 mx-0 mb-2 bg-light">
+  <div class="col-sm-12" align="center">
+ 	<div class="form-group">
+
+    <div class="col-sm-10">
+     <div class="input-group input-group-md">
+     <div class="input-group-prepend">
+	    <button class="btn btn-success px-5" type="submit" name="submitrekap">Enter Data</button>
+	 </div>
+	 <label class="control-label col-sm-8 align-self-center text-center mb-0" for="dateinp2">
+
+	 </label>
+	 </div> 
+ 	</div>
+
+ 	</div>
+  </div>
 </div>
