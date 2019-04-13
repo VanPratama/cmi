d83c4a97d10eed867a2cb32521459e78d826ef6d
VanP <p.ivan000.ip@gmail.com>
2019-04-13 10:59:26 +0700

    blueprint

---
+++ tablerekap.php
@@ -0,0 +1,46 @@
+<div class="row r5 mx-0 mb-1 bg-light px-3 py-4 table-responsive">
+ <div id="alert_message"></div>
+ <table  id="rekaptable" class="table table-striped table-bordered text-center">
+ 	<thead class="bg-success" style="font-size: 15px;">
+ 	<tr>
+ 		<th rowspan="2">ID</th>
+     	<th rowspan="2">COA</th>
+     	<th rowspan="2">Nama Customer</th>
+     	<th rowspan="2">Nama Barang</th>
+ 		<th colspan="5">SPK</th>
+ 		<th colspan="6">Invoice</th>
+ 		<th colspan="4">Payment</th>
+ 		<th colspan="2">Mesin</th>
+ 		<th colspan="4">Shift</th>
+ 		<th rowspan="2">HPP</th>
+ 	</tr>
+ 	 <tr>
+     	<!-- <th>Total Rekap</th> -->
+     	<th>SPK Date</th>
+     	<th>No. SPK</th>
+     	<th>PO Qty</th>
+     	<th>Hasil</th>
+     	<th>Selisih</th>
+     	<th>Invoice Date</th>
+     	<th>No.Invoice</th>
+     	<th>PO Qty</th>
+     	<th>P/U</th>
+     	<th>Nominal</th>
+     	<th>Total</th>
+     	<th>Pay Date</th>
+     	<th>Descr.</th>
+     	<th>Pay Total</th>
+     	<th>Saldo</th>
+     	<th>Besar</th>
+     	<th>Kecil</th>
+     	<th>Pagi</th>
+     	<th>Malam</th>
+     	<th>Sabtu Pagi</th>
+     	<th>Sabtu Malam</th>
+     </tr>
+ 	</thead>	
+ 	<tbody id="tb" style="text-transform: capitalize;font-size: 12px;">
+ 	 <!-- <?php include 'ViewTable.php'?> -->
+ 	</tbody> 
+ </table>
+</div>
